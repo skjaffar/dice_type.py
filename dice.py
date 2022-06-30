e9ac9bed271b6f82465932fd1e3b48f81cb21cb2
Shaik Jaffar <107207838+skjaffar@users.noreply.github.com>
2022-06-30 17:56:34 +0530

    Add files via upload

---
+++ dice.py
@@ -0,0 +1,25 @@
+
+# d={1:"", 2: 36, 3: "munna", 4: [6.9], 5: (9,6),
+# 6: {3: "world"}}
+# print(d[4])
+# print(d,type(d))
+# print(d.keys())
+# print(d.items())
+
+# k={1: "", 2: "moulali", 3: 2.1, 4: 7, 4: [8.3], 5: (96),6: {3: 9,}}
+# k.clear()
+# print(k)
+
+# a=[12,17,96,26,85,63]
+# print(dict.fromkeys(a,15))
+
+# n={1: "hai", 2: 96, 3: 9.3, 4: (8.9), 5: [99], 6:{6: 96}}
+# print(n.get(2))
+
+# n={1: "hai", 2: 96, 3: 9.3, 4: (8.9), 5: [99], 6:{6: 96}}
+# n.setdefault(7,'hai')
+# print(n)
+# n.setdefault(8, 'myname')
+# print(n)
+
+
